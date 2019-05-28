df6a4a80c97b4e35691577dddd8325047d6f898a
jeremyNCH <jnchafok@uwaterloo.ca>
2019-05-27 23:56:08 -0400

    beforeEach hook for contract test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -13,7 +13,22 @@ let campaign;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
+
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledCampaign.bytecode })
     .send({ from: accounts[0], gas: '1000000' });
+
+  await factory.methods.createCampaign('100').send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+
+  // ES2016 - array destructuring -> assign arr[0] to campaignAddress
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+  // at this point, campaign already deployed, just specify ABI and address to get ref
+  campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
 });
