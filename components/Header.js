a36b7d2aead9458691d90cdfefcae5f03570906c
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-01 18:37:58 -0400

    adding new campaign page and refactoring layout for CDN s.sheet

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,13 @@ import { Menu, Icon } from 'semantic-ui-react';
 
 export default () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: '10px' }}>
       <Menu.Item>INDIEGOGO</Menu.Item>
 
       <Menu.Menu position="right">
         <Menu.Item>Campaigns</Menu.Item>
         <Menu.Item>
-          <Icon name="plus" />
+          <Icon style={{ margin: 0, padding: 0 }} name="plus" />
         </Menu.Item>
       </Menu.Menu>
     </Menu>
