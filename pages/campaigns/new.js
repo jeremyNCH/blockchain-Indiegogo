e77ad12959bccd35d5302222b6ab997e8e6bfa87
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-01 19:36:22 -0400

    wire create campaign to the smart contract and adding validation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: '',
+    errorMsg: ''
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({ from: accounts[0] });
+    } catch (err) {
+      this.setState({ errorMsg: err.message });
+    }
+  };
+
   render() {
     return (
       <Layout>
         <h3>Create a Campaign</h3>
 
-        <Form>
+        <Form onSubmit={this.onSubmit} error={Boolean(this.state.errorMsg)}>
           <Form.Field>
             <label>Minimum Contribution</label>
-            <input />
+            <Input
+              type="number"
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={e =>
+                this.setState({ minimumContribution: e.target.value })
+              }
+            />
           </Form.Field>
+          <Message error header="Oops!" content={this.state.errorMsg} />
           <Button primary>Create</Button>
         </Form>
       </Layout>
