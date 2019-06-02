9c392886885cdf08468ed28f5e9cc527043086e9
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-01 23:05:14 -0400

    Redirect to home after creating new campaign and adding favicon

--- components/Layout.js
+++ components/Layout.js
@@ -11,6 +11,7 @@ export default props => {
           rel="stylesheet"
           href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
         />
+        <link rel="shortcut icon" href="/static/favicon.ico" />
       </Head>
       <Header />
       {props.children}
