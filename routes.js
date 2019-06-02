667bea3a6f78239f4a385df45c41df700ce1ba79
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-01 23:28:34 -0400

    adding routes for CampaignShow page - add route rules for url tokens

--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
 const routes = require('next-routes')();
 
+// order of routes is important
+routes
+  .add('/campaigns/new', '/campaigns/new')
+  .add('/campaigns/:address', '/campaigns/show');
+
 module.exports = routes;
