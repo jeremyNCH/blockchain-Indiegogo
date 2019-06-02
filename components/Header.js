8eb30f3bb2465a13110c34c60bf8d576b4c00b22
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-01 23:12:41 -0400

    wiring up header links - Menu.Item -> Link as semantic-ui style clash

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
+import { Link } from '../routes';
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
-      <Menu.Item>INDIEGOGO</Menu.Item>
+      <Link route="/">
+        <a className="item">INDIEGOGO</a>
+      </Link>
 
       <Menu.Menu position="right">
-        <Menu.Item>Campaigns</Menu.Item>
-        <Menu.Item>
-          <Icon style={{ margin: 0, padding: 0 }} name="plus" />
-        </Menu.Item>
+        <Link route="/">
+          <a className="item">Campaigns</a>
+        </Link>
+
+        <Link route="/campaigns/new">
+          <a className="item">
+            <Icon style={{ margin: 0, padding: 0 }} name="plus" />
+          </a>
+        </Link>
       </Menu.Menu>
     </Menu>
   );
