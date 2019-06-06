4cd04e904f8c7f73bc0eab7f3aa3056f076a478c
jeremyNCH <jnchafok@uwaterloo.ca>
2019-06-06 00:05:52 -0400

    adding custom css to requestRow

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -2,28 +2,53 @@ import React, { Component } from 'react';
 import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
 import Campaign from '../ethereum/campaign';
+import { Router } from '../routes';
 
 class RequestRow extends Component {
+  state = { loadingApprove: false, loadingFinalize: false, errMsg: '' };
+
   onApprove = async () => {
-    const campaign = Campaign(this.props.address);
-    const accounts = await web3.eth.getAccounts();
-    await campaign.methods.approveRequest(this.props.id).send({
-      from: accounts[0]
-    });
+    console.log(`/campaigns/${this.props.address}/requests`);
+    this.setState({ loadingApprove: true, errorMsg: '' });
+
+    try {
+      const campaign = Campaign(this.props.address);
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.approveRequest(this.props.id).send({
+        from: accounts[0]
+      });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMsg: err.message });
+    }
+
+    this.setState({ loadingApprove: false });
   };
 
   onFinalize = async () => {
-    const campaign = Campaign(this.props.address);
-    const accounts = await web3.eth.getAccounts();
-    await campaign.methods.finalizeRequest(this.props.id).send({
-      from: accounts[0]
-    });
+    this.setState({ loadingFinalize: true, errorMsg: '' });
+
+    try {
+      const campaign = Campaign(this.props.address);
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.finalizeRequest(this.props.id).send({
+        from: accounts[0]
+      });
+
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+    } catch (err) {
+      this.setState({ errorMsg: err.message });
+    }
+
+    this.setState({ loadingFinalize: false });
   };
 
   render() {
     const { Row, Cell } = Table;
     const { id, request, approversCount } = this.props;
     const readyToFinalize = request.approvalCount > approversCount / 2;
+    const allApproved = request.approvalCount === approversCount;
 
     return (
       <Row
@@ -36,20 +61,29 @@ class RequestRow extends Component {
         <Cell>{request.recipient}</Cell>
         <Cell>{`${request.approvalCount}/${approversCount}`}</Cell>
         <Cell>
-          {request.complete ? null : (
-            <Button basic color="green" onClick={this.onApprove}>
+          {request.complete || allApproved ? null : (
+            <Button
+              basic
+              color="green"
+              loading={this.state.loadingApprove}
+              onClick={this.onApprove}
+            >
               Approve
             </Button>
           )}
         </Cell>
         <Cell>
           {request.complete ? null : (
-            <Button basic color="teal" onClick={this.onFinalize}>
+            <Button
+              basic
+              color="teal"
+              loading={this.state.loadingFinalize}
+              onClick={this.onFinalize}
+            >
               Finalize
             </Button>
           )}
         </Cell>
-        <Cell>{}</Cell>
       </Row>
     );
   }
